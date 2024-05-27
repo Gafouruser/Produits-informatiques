2aef9088305f851309327ac76fdf6c29a2f85499
HoungboRoberto <robertohoungbo@gmail.com>
2024-05-27 14:59:22 +0100

    création enums.py et modification models.py

---
+++ product_catalog/products/enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Category(Enum):
+    APPLICATION = "Application"
+    HARDWARE = "Matériel"
+    OS = "Système d'exploitation"
--- product_catalog/products/models.py
+++ product_catalog/products/models.py
@@ -1,20 +1,11 @@
 # products/models.py
 from django.db import models
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
+from .enums import Category
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    cpe_id = models.CharField(max_length=255, unique=True)
+    vendor = models.CharField(blank=True, null=True)
+    category = models.CharField(choices=[(category.value, category.name) for category in Category], max_length=30)
     version = models.CharField(max_length=50, blank=True, null=True)
-    release_date = models.DateField(blank=True, null=True)
     website = models.URLField(blank=True, null=True)
 
     def __str__(self):
