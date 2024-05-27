82df2fb320e73d3b69373c1dd327a91c8640132c
Gafouruser <yekiniabdougafour@gmail.com>
2024-05-27 17:30:48 +0100

    Ajout de la pagination

--- product_catalog/products/views.py
+++ product_catalog/products/views.py
@@ -1,11 +1,17 @@
-# products/views.py
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Product
 
 def product_list(request):
     query = request.GET.get('q')
     if query:
-        products = Product.objects.filter(name__icontains=query)
+        products_list = Product.objects.filter(name__icontains=query)
     else:
-        products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': products})
+        products_list = Product.objects.all()
+    
+    paginator = Paginator(products_list, 50)  # 50 éléments par page
+    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'products/product_list.html', {'page_obj': page_obj, 'query': query})
