a76be064081eb129c0960c6108674ca6be19a819
HOUNGBETODE <moberenge@gmail.com>
2024-05-28 18:23:20 +0200

    Update fill_db.py

--- product_catalog/scripts/fill_db.py
+++ product_catalog/scripts/fill_db.py
@@ -1,7 +1,7 @@
 import re
 import requests
 from bs4 import BeautifulSoup
-from products.models import Product
+from products.models import Product, Vulnerability
 from products.enums import Category
 
 def run():
@@ -29,27 +29,36 @@ def run():
                         """ #This is the format of a CPE...
 						cpe:<cpe_version>:<part>:<vendor>:<product>:<version>:<update>:<edition>:<language>:<sw_edition>:<target_sw>:<target_hw>:<other>
 						"""
-                        cpe_version = cpe_parts[1]
                         part = cpe_parts[2] # a => application, h => hardware, o => operating systems [part refers to cpe_type]
                         vendor = cpe_parts[3]
                         product = cpe_parts[4]
                         version = cpe_parts[5]
-                        update = cpe_parts[6]
-                        edition = cpe_parts[7]
-                        language = cpe_parts[8]
-                        sw_edition = cpe_parts[9]
-                        target_sw = cpe_parts[10]
-                        target_hw = cpe_parts[11]
-                        other = cpe_parts[12]
 
                         category = Category.APPLICATION.value if part=='a' else Category.HARDWARE.value if part=="h" else Category.OS.value
 						
                         cpe_detail = {'name':product, 'vendor':vendor, 'category':category, 'version':version, 'cpe_name':tagContent, 'website':cpe_url}
 
-                        Product.objects.create(**cpe_detail)
+                        product_instance = Product.objects.create(**cpe_detail)
+                        retrieveCVEsPerCPE_(product_instance, tagContent, product)
                 else:
                     break
 				
                 print(f"{current_index = }")
 			# l'index s'incrémente par bond de 20
             current_index += 20
+
+
+def retrieveCVEsPerCPE_(product_object, product_cpe_name, product_name):    
+    nvd_api = f"https://services.nvd.nist.gov/rest/json/cves/2.0?cpeName={product_cpe_name}"
+
+    session = requests.Session()
+    nvd_api_response = session.get(nvd_api).json()
+    vulnerabilities = nvd_api_response['vulnerabilities']
+
+    for aVulnDetail in vulnerabilities:
+        cve_section = aVulnDetail['cve']
+        cve_id = cve_section['id']
+        cve_english_description = [language['value'] for language in cve_section['descriptions'] if language['lang']=='en'][0]
+        product_object.vulnerabilities.add(Vulnerability.objects.get_or_create(name=cve_id, description=cve_english_description)[0])
+    print("     CVEs added....")
+    session.close()
